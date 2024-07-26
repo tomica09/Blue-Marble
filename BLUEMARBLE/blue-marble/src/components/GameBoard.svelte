5d2038dd67f0236ff8976df3230d0d44cc08a0da
tomica09 <tomica09@naver.com>
2024-07-26 11:16:37 +0900

    d

---
+++ BLUEMARBLE/blue-marble/src/components/GameBoard.svelte
@@ -0,0 +1,28 @@
+<script>
+    import Tile from './Tile.svelte';
+    
+    // 예제 타일 데이터
+    const tiles = [
+      { id: 1, name: 'Start' },
+      { id: 2, name: 'Property 1' },
+      // 추가 타일 데이터
+    ];
+  </script>
+  
+  <style>
+    .board {
+      display: grid;
+      grid-template-columns: repeat(10, 1fr);
+      grid-template-rows: repeat(10, 1fr);
+      gap: 1px;
+      width: 100%; 
+      height: 100%;
+    }
+  </style>
+  
+  <div class="board">
+    {#each tiles as tile}
+      <Tile {tile} />
+    {/each}
+  </div>
+  
